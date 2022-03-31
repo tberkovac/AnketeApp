f61407fb9bc24e3bdf344b75c9e534642a8b0c05
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-03-31 23:50:32 +0200

    Dodano jos pocetnih anketa

--- app/src/main/java/ba/etf/rma22/projekat/data/AnketaStaticData.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/AnketaStaticData.kt
@@ -5,15 +5,15 @@ import java.util.*
 
 fun dajAnketeStatic(): List<Anketa>{
 
-    var cal: Calendar = Calendar.getInstance()
+    val cal: Calendar = Calendar.getInstance()
     cal.set(2022,3,10)
-    var date: Date = cal.time;
+    val date: Date = cal.time
     cal.set(2022, 5, 10)
-    var date2: Date = cal.time;
+    val date2: Date = cal.time
     cal.set(2022, 5, 19)
-    var date3: Date = cal.time;
+    val date3: Date = cal.time
     cal.set(2022,3,20)
-    var date4: Date = cal.time;
+    val date4: Date = cal.time
 
     return listOf(
         Anketa("Anketa 1","Istrazivanje 1", date , date2, null,10,
@@ -24,5 +24,13 @@ fun dajAnketeStatic(): List<Anketa>{
             "treca grupica", 0.6f), //treba biti ispunjena tj plava
         Anketa("Anketa 4","Istrazivanje 4", date , date4, null,10,
             "cetvrta grupica", 0.4f), //treba biti zuto
+        Anketa("Anketa 5","Istrazivanje 5", date2 , date3, date2,10,
+            "peta grupica", 0.6f), //treba biti ispunjena tj plava
+        Anketa("Anketa 6","Istrazivanje 6", date , date4, null,10,
+            "sesta grupica", 0.4f), //treba biti zuto
+        Anketa("Anketa 7","Istrazivanje 7", date2 , date3, date2,10,
+            "sedma grupica", 0.6f), //treba biti ispunjena tj plava
+        Anketa("Anketa 8","Istrazivanje 8", date , date4, null,10,
+            "osma grupica", 0.4f), //treba biti zuto
     )
 }
