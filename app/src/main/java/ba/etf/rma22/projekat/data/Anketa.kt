4d9f9f3b57da60891af2bc2edc56fd564e746916
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-03-31 14:35:26 +0200

    Dodana data class Anketa

---
+++ app/src/main/java/ba/etf/rma22/projekat/data/Anketa.kt
@@ -0,0 +1,14 @@
+package ba.etf.rma22.projekat.data
+
+import java.util.*
+
+data class Anketa(
+    val naziv: String,
+    val nazivIstraživanja: String,
+    val datumPocetak: Date,
+    val datumKraj: Date,
+    val datumRada: Date?,
+    val trajanje: Int,
+    val nazivGrupe: String,
+    val progres: Float
+)
