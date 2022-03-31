ceee10321f735d3f73c6655cdcc88cc1119a70f3
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-03-31 23:48:20 +0200

    Dodane funkcije getAll, getDone, getFuture, getNotTaken

--- app/src/main/java/ba/etf/rma22/projekat/data/AnketaRepository.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/AnketaRepository.kt
@@ -1,8 +1,30 @@
 package ba.etf.rma22.projekat.data
 
+import java.time.LocalDate
+import java.util.*
+
 
 object AnketaRepository {
-    fun getAnkete() : List<Anketa> {
+    fun getMyAnkete() : List<Anketa> {
         return dajAnketeStatic()
+    }
+
+    fun getAll(): List<Anketa> {
+        return dajAnketeStatic()
+    }
+
+    fun getDone(): List<Anketa> {
+        return dajAnketeStatic().filter{anketa ->  anketa.datumRada!=null}.toList()
+    }
+
+    fun getFuture(): List<Anketa> {
+        val cal: Calendar = Calendar.getInstance()
+        cal.set(LocalDate.now().year, LocalDate.now().monthValue, LocalDate.now().dayOfMonth)
+        val date: Date = cal.time
+        return getAll().filter { anketa -> anketa.datumPocetak> date}
+    }
+
+    fun getNotTaken(): List<Anketa> {
+        return getAll().filter{ anketa -> anketa.datumRada == null }
     }
 }
