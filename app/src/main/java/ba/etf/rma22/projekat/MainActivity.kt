a13e389b59f74fbebfd82ad79c5dafaa610251b7
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-04-01 01:27:35 +0200

    Dodan atribut i akcija za floating button Spiner je sada funkcionalan

--- app/src/main/java/ba/etf/rma22/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma22/projekat/MainActivity.kt
@@ -1,17 +1,24 @@
 package ba.etf.rma22.projekat
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Spinner
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma22.projekat.view.AnketaListAdapter
 import ba.etf.rma22.projekat.viewmodel.AnketeListViewModel
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var spinerFilter: Spinner
-    private lateinit var listaAnketa: RecyclerView
-    private lateinit var listaAnketaAdapter: AnketaListAdapter
+    private lateinit var spinerFilter : Spinner
+    private lateinit var listaAnketa : RecyclerView
+    private lateinit var listaAnketaAdapter : AnketaListAdapter
+    private lateinit var upisIstrazivanja : FloatingActionButton
     private var anketeListViewModel = AnketeListViewModel()
 
 
@@ -21,13 +28,56 @@ class MainActivity : AppCompatActivity() {
 
         spinerFilter = findViewById(R.id.filterAnketa)
         listaAnketa = findViewById(R.id.listaAnketa)
+        upisIstrazivanja = findViewById(R.id.upisDugme)
 
         listaAnketa.layoutManager = GridLayoutManager(
-            this, 2
+            this, 2, GridLayoutManager.VERTICAL, false
         )
         listaAnketaAdapter = AnketaListAdapter(arrayListOf())
 
         listaAnketa.adapter = listaAnketaAdapter
         listaAnketaAdapter.updateAnkete(anketeListViewModel.getAnkete())
+
+        upisIstrazivanja.setOnClickListener {
+            val intent = Intent(this, UpisIstrazivanje::class.java)
+            startActivity(intent)
+        }
+
+        val podaciZaSpinner = arrayOf(
+            "Sve moje ankete",
+            "Sve ankete",
+            "Urađene ankete",
+            "Buduće ankete",
+            "Prošle ankete"
+        )
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_spinner_item, podaciZaSpinner
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinerFilter.adapter = adapter
+        spinerFilter.setSelection(1)
+        spinerFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                val vrijednost : String = podaciZaSpinner[p2]
+
+                if(vrijednost == podaciZaSpinner[1])
+                    listaAnketaAdapter.updateAnkete(anketeListViewModel.getAnkete())
+                else if(vrijednost == podaciZaSpinner[2])
+                    listaAnketaAdapter.updateAnkete(anketeListViewModel.getDone())
+                else if(vrijednost == podaciZaSpinner[3])
+                    listaAnketaAdapter.updateAnkete(anketeListViewModel.getFuture())
+                else if(vrijednost == podaciZaSpinner[4])
+                    listaAnketaAdapter.updateAnkete(anketeListViewModel.getNotTaken())
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
+        }
+
     }
+
 }
