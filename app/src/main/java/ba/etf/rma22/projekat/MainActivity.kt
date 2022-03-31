1c174264c36fc6811ebaf85dc51aa1151e13f7e9
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-03-31 19:22:22 +0200

    Dodani atributi za View-e na activity_main i vrsi se punjenje RecyclerView-a

--- app/src/main/java/ba/etf/rma22/projekat/MainActivity.kt
+++ app/src/main/java/ba/etf/rma22/projekat/MainActivity.kt
@@ -2,10 +2,32 @@ package ba.etf.rma22.projekat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Spinner
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ba.etf.rma22.projekat.view.AnketaListAdapter
+import ba.etf.rma22.projekat.viewmodel.AnketeListViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var spinerFilter: Spinner
+    private lateinit var listaAnketa: RecyclerView
+    private lateinit var listaAnketaAdapter: AnketaListAdapter
+    private var anketeListViewModel = AnketeListViewModel()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        spinerFilter = findViewById(R.id.filterAnketa)
+        listaAnketa = findViewById(R.id.listaAnketa)
+
+        listaAnketa.layoutManager = GridLayoutManager(
+            this, 2
+        )
+        listaAnketaAdapter = AnketaListAdapter(arrayListOf())
+
+        listaAnketa.adapter = listaAnketaAdapter
+        listaAnketaAdapter.updateAnkete(anketeListViewModel.getAnkete())
     }
 }
