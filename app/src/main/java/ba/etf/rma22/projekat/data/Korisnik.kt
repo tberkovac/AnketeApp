e24125d92002ba701dc2bd34a653b77eabaab437
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-04-03 01:42:27 +0200

    Dodana pocetna upisana istrazivanja/grupe

--- app/src/main/java/ba/etf/rma22/projekat/data/Korisnik.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/Korisnik.kt
@@ -2,9 +2,12 @@ package ba.etf.rma22.projekat.data
 
 class Korisnik(
     val ime: String = "Testni korisnik",
-    var upisanaIstrazivanja: List<Istrazivanje> = listOf(Istrazivanje("lagano istrazivanje",1),
-                                                        Istrazivanje("zadovoljavajuce istrazivanje", 2)
+    var upisanaIstrazivanja: List<Istrazivanje> = listOf(
+        Istrazivanje("lagano istrazivanje", 1),
+        Istrazivanje("zadovoljavajuce istrazivanje", 2)
     ),
-    var upisaneGrupe: List<Grupa> = listOf(Grupa("grupica 1","lagano istrazivanje"),
-                                            Grupa("grupica 2","zadovoljavajuce istrazivanje"))
+     var upisaneGrupe: List<Grupa> = listOf(
+        Grupa("grupica 1", "lagano istrazivanje"),
+        Grupa("grupica 2", "zadovoljavajuce istrazivanje")
+    )
 )
