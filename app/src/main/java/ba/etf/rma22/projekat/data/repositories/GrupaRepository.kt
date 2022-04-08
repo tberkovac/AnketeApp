7cebfae92b28ebd933b21303d220e3be163bb99b
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-04-09 00:54:02 +0200

    Implementacija funkcija

--- app/src/main/java/ba/etf/rma22/projekat/data/repositories/GrupaRepository.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/repositories/GrupaRepository.kt
@@ -4,7 +4,8 @@ import ba.etf.rma22.projekat.data.models.Grupa
 import ba.etf.rma22.projekat.data.dajGrupeStatic
 
 object GrupaRepository {
-    fun getGroupsByIstrazivanje(nazivIstrazivanja:String) : List<Grupa>{
-        return dajGrupeStatic()
+    fun getGroupsByIstrazivanje(nazivIstrazivanja : String) : List<Grupa>{
+        return dajGrupeStatic().filter { grupa -> grupa.nazivIstrazivanja.equals(nazivIstrazivanja) }
+            .ifEmpty { emptyList() }
     }
 }
--- app/src/main/java/ba/etf/rma22/projekat/data/repositories/IstrazivanjeRepository.kt
+++ app/src/main/java/ba/etf/rma22/projekat/data/repositories/IstrazivanjeRepository.kt
@@ -2,17 +2,22 @@ package ba.etf.rma22.projekat.data.repositories
 
 import ba.etf.rma22.projekat.data.models.Istrazivanje
 import ba.etf.rma22.projekat.data.dajIstrazivanjaStaticData
+import ba.etf.rma22.projekat.data.models.Korisnik
 
 object IstrazivanjeRepository {
+    val korisnik = Korisnik()
+
     fun getIstrazivanjeByGodina(godina:Int) : List<Istrazivanje>{
         return dajIstrazivanjaStaticData().filter { istrazivanje -> istrazivanje.godina == godina }
+            .ifEmpty { emptyList() }
     }
 
     fun getAll() : List<Istrazivanje> {
-        return dajIstrazivanjaStaticData()
+        return dajIstrazivanjaStaticData().ifEmpty { emptyList() }
     }
 
     fun getUpisani() : List<Istrazivanje>{
-        return listOf()
+        return dajIstrazivanjaStaticData().filter { istrazivanje -> korisnik.companion.upisanaIstrazivanja.contains(istrazivanje) }
+            .ifEmpty { emptyList() }
     }
 }
