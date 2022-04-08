8c69b0bd67d0d123312c22e0a6b73871588c6c64
Tarik Berkovac <tberkovac1@etf.unsa.ba>
2022-04-09 00:55:11 +0200

    Obrisan KorisnikStaticData fajl

--- app/src/main/java/ba/etf/rma22/projekat/data/staticdata/KorisnikStaticData.kt
+++
@@ -1,9 +0,0 @@
-package ba.etf.rma22.projekat.data
-
-import ba.etf.rma22.projekat.data.models.Istrazivanje
-
-fun dajUpisanaIstrazivanjaKorisnika() : List<Istrazivanje> {
-    return listOf(
-        Istrazivanje("lagano istrazivanje", 1),
-    )
-}
