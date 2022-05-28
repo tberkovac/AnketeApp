489a2c916476dfafa1242e36e8c7848e0dddcbc2
tberkovac <tberkovac1@etf.unsa.ba>
2022-05-28 18:51:16 +0200

    Obrisani fajlovi vezani za static data i response klase koje ne trebaju

--- app/src/main/java/ba/etf/rma22/projekat/data/models/GetIstrazivanjeResponse.kt
+++
@@ -1,7 +0,0 @@
-package ba.etf.rma22.projekat.data.models
-
-import com.google.gson.annotations.SerializedName
-
-data class GetIstrazivanjeResponse (
-    @SerializedName("istrazivanja") val istrazivanja : List<Istrazivanje>,
-)
